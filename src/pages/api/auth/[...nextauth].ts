dcde40667b098e3b972383f56d7140e8f5ec76e9
Rafael Aragão <46876806+rafaelfaragao@users.noreply.github.com>
2021-09-23 22:08:31 -0300

    Add FaunaDB config, add create user code on next-auth api

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,4 +15,40 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const {email} = user
+      
+      try {
+        await fauna.query(
+          q.If( // SE (Condição, TRUE, FALSE)
+            q.Not( // CONDIÇÂO
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ), // fim da condição, primeira virgula
+            q.Create( // TRUE
+              q.Collection('users'),
+              {data: {email}}
+            ), // segunda virgula
+            q.Get( // FALSE
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        );
+        return true;
+
+      } catch (error){
+        console.log(error);
+        return false;
+      }
+
+    },
+  }
 })
