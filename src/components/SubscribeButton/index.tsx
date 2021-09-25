7828c143330b5ea70d528836784e2a5961860b26
Rafael Aragão <46876806+rafaelfaragao@users.noreply.github.com>
2021-09-24 23:00:35 -0300

    add chekoutSession page Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,43 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
+//
+
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe(){
+    if(!session){
+      signIn('github')
+      return;
+    }
+
+    // se existir a session realizar a criação da checkout sessão
+    try { 
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs() // stripeJS para usar o stripe no front
+
+      await stripe.redirectToCheckout({sessionId: sessionId})
+    } catch (err) {
+      alert(err.message);
+    }
+
+  }
+
   return (
     <button
      type="button"
      className={styles.subscribeButton}
+     onClick={handleSubscribe}
     >
       Subscribe now
     </button>
